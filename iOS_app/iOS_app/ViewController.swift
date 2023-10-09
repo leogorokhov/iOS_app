039d9108a4f9b2d0af259f6fb49a17b8bf618dd1
Leo <lenia.goroxow@yandex.ru>
2023-10-09 14:13:18 +0300

    created project

---
+++ iOS_app/iOS_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_app
+//
+//  Created by Леонид Горохов on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
