b9f1a32eb1e90a971ec7ce87b2b166d9f7cd7c0c
Leo <lenia.goroxow@yandex.ru>
2023-10-30 18:10:12 +0300

    added tableview

--- iOS_app/ViewController.swift
+++ iOS_app/ViewController.swift
@@ -6,27 +6,59 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let drink = drinkData?.drinks else { return UITableViewCell() }
+        let cell = UITableViewCell()
+        var configuration = cell.defaultContentConfiguration()
+        // configuration.image = UIImage()
+        configuration.text = drink[indexPath.row].strDrink
+        configuration.secondaryText = drink[indexPath.row].idDrink
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    private var drinkData: Root?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemPink
+        view.addSubview(tableView)
+        tableView.backgroundColor = .systemGray
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
         
         let url: URL = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/random.php")!
         URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
             guard
                 let data,
-                let response,
                 error == nil
             else {
                     return
             }
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let model = try! decoder.decode(Root.self, from: data)
-            print(model)
+            self.drinkData = try! decoder.decode(Root.self, from: data)
+
+            DispatchQueue.main.async(execute: {
+                self.tableView.reloadData()
+            })
         }).resume()
         
     }
